fdd9e87ccdd12a4647f080971335867366717fd8
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-05-08 16:13:35 -0500

    subdominio

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    basePath: '/safepassdocs',
+    trailingSlash: true
+  };
+  
+  module.exports = nextConfig;
+  
